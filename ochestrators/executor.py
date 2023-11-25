79cd4b19fbfa61d74c1fdca0b2c437f82b486204
cmdrGuyson <50499511+cmdrGuyson@users.noreply.github.com>
2023-11-25 22:50:22 +0530

    feat: skip newletter on weekend

--- ochestrators/executor.py
+++ ochestrators/executor.py
@@ -2,7 +2,7 @@ from modules.currency import CurrenyModule
 from modules.email import EmailModule
 from utils.logger import Logger
 from templates.newsletter import main_template
-from datetime import datetime
+from datetime import datetime, date
 
 
 class Executor:
@@ -13,7 +13,14 @@ class Executor:
 
     def execute_job(self):
         """Execute newsletter job"""
+
         self.logger.info("Executing newsletter job")
+
+        today = date.today()
+        if not today.isoweekday() < 6:
+            self.logger.info("Skipping newsletter execution on weekend")
+            return
+
         try:
             self.logger.info("Successfully executed newsletter job")
             rates = self.currency_module.plot_currency_graph()
@@ -22,15 +29,15 @@ class Executor:
                 raise Exception("No rates found")
 
             current_rate = rates[-1].get("rate")
-            today = datetime.now().strftime("%dth %B %Y")
+            current_day = datetime.now().strftime("%dth %B %Y")
 
             if not current_rate:
                 raise Exception("No current currency rate")
 
-            html_content = main_template.format("Gayanga", today, current_rate)
+            html_content = main_template.format("Gayanga", current_day, current_rate)
 
             self.email_module.set_html_content(html_content)
-            self.email_module.set_subject(f"Henchman | {today}")
+            self.email_module.set_subject(f"Henchman | {current_day}")
             self.email_module.attach_image(
                 "/Users/guyson/Documents/Work/Henchman/temp/figure.png", "graph"
             )
